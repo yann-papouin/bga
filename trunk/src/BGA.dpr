22b0a1f68b771689444ec242344df934187c006f
yann.papouin <yann.papouin@c3e53976-ae9b-ca61-d8de-0576eba5e183>
2010-05-07 18:56:36 +0000

    Adding progress events for future gui use Cleaning

--- trunk/src/BGA.dpr
+++ trunk/src/BGA.dpr
@@ -24,7 +24,7 @@ uses
 {$R *.res}
 
 begin
-  ReportMemoryLeaksOnShutdown := DebugHook <> 0;
+  //ReportMemoryLeaksOnShutdown := DebugHook <> 0;
 
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
