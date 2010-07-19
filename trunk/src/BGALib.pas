b7a8f9303420d7f8b285c26fe59986d820149057
yann.papouin <yann.papouin@c3e53976-ae9b-ca61-d8de-0576eba5e183>
2010-07-19 17:59:12 +0000

    [Fix]Invalid float data parsing

--- trunk/src/BGALib.pas
+++ trunk/src/BGALib.pas
@@ -2,10 +2,39 @@ unit BGALib;
 
 interface
 
+uses
+  SysUtils;
+
 type
 
   TBgaGetFileByPath = function(Sender: TObject; const VirtualPath : string) : string of object;
 
+  procedure EnableBFDecimal;
+  procedure DisableBFDecimal;
+
 implementation
 
+const
+  BATTLEFIELD_DECIMAL = '.';
+
+var
+  SystemDecimalSeparator : Char;
+
+  procedure EnableBFDecimal;
+  begin
+    if SystemDecimalSeparator = #0 then
+    begin
+      SystemDecimalSeparator := DecimalSeparator;
+      DecimalSeparator := BATTLEFIELD_DECIMAL;
+    end;
+  end;
+
+  procedure DisableBFDecimal;
+  begin
+    if SystemDecimalSeparator <> #0 then
+    begin
+      DecimalSeparator := SystemDecimalSeparator;
+      SystemDecimalSeparator := #0;
+    end;
+  end;
 end.
